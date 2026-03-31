176556f05249996a1ca79dd812d39181db437042
Chando64 <famhernanrodri09@gmail.com>
2026-03-31 09:35:13 -0700

    The interactive map has been changed so that it shows the percentage of votes for 2020 elections for priz and senate. The map for virgina only works so far.

--- Elections of two states.R
+++ Elections of two states.R
@@ -11,11 +11,16 @@ plans_fl <- alarm_50state_plans('FL')
 map_va <- alarm_50state_map('VA')
 plans_va <- alarm_50state_plans('VA')
 
-#hdjhhdfgah
 
 map_va |> 
   ggplot() +
-  geom_district(aes(group = cd_2020, fill = ndv, denom = nrv + ndv)) +
+  geom_district(aes(group = cd_2020, fill = adv_16, denom = arv_16 + adv_16)) +
+  scale_fill_party_c() +
+  theme_map()
+
+map_va |> 
+  ggplot() +
+  geom_district(aes(group = cd_2020, fill = ndv, denom = ndv + nrv)) +
   scale_fill_party_c() +
   theme_map()
 
@@ -29,46 +34,36 @@ map_fl |>
 popu_va_1 <- map_va|>
   group_by(cd_2020) |>
   summarise(
-    prop_hisp = (sum(pop_hisp)/sum(pop))*100,
-    prop_white=(sum(pop_white)/sum(pop))*100,
-    prop_black=(sum(pop_black)/sum(pop))*100,
-    prop_aian=(sum(pop_aian)/sum(pop))*100,
-    prop_asian=(sum(pop_asian)/sum(pop))*100,
-    prop_nhpi=(sum(pop_nhpi)/sum(pop))*100,
-    prop_other=(sum(pop_other)/sum(pop))*100,
-    prop_two=(sum(pop_two)/sum(pop))*100,
+    prop_priz_16_cli = (sum(pre_16_dem_cli)/(sum(pre_16_dem_cli)+sum(pre_16_rep_tru)))*100,
+    prop_priz_16_tru=(sum(pre_16_rep_tru)/(sum(pre_16_dem_cli)+sum(pre_16_rep_tru)))*100,
+    prop_uss_18_kai=(sum(uss_18_rep_ste)/(sum(uss_18_dem_kai)+sum(uss_18_rep_ste)))*100,
+    prop_uss_18_ste=(sum(uss_18_dem_kai)/(sum(uss_18_dem_kai)+sum(uss_18_rep_ste)))*100,
+    prop_priz_20_bid=(sum(pre_20_dem_bid)/(sum(pre_20_dem_bid)+sum(pre_20_rep_tru)))*100,
+    prop_priz_20_tru=(sum(pre_20_rep_tru)/(sum(pre_20_dem_bid)+sum(pre_20_rep_tru)))*100,
+    prop_uss_20_war=(sum(uss_20_dem_war)/(sum(uss_20_dem_war)+sum(uss_20_rep_gad)))*100,
+    prop_uss_20_ga=(sum(uss_20_rep_gad)/(sum(uss_20_dem_war)+sum(uss_20_rep_gad)))*100,
     vote_share = sum(ndv) / (sum(ndv) + sum(nrv))
   )
 
-popu_fl_1 <- map_fl|>
-  st_make_valid() |>
-  group_by(cd_2020) |>
-  summarise(
-    prop_hisp = (sum(pop_hisp)/sum(pop))*100,
-    prop_white=(sum(pop_white)/sum(pop))*100,
-    prop_black=(sum(pop_black)/sum(pop))*100,
-    prop_aian=(sum(pop_aian)/sum(pop))*100,
-    prop_asian=(sum(pop_asian)/sum(pop))*100,
-    prop_nhpi=(sum(pop_nhpi)/sum(pop))*100,
-    prop_other=(sum(pop_other)/sum(pop))*100,
-    prop_two=(sum(pop_two)/sum(pop))*100,
-    vote_share = sum(ndv) / (sum(ndv) + sum(nrv))
-  )
 ###########################################################
 map_va_in <- popu_va_1 |>
   ggplot(aes(fill = vote_share,text=paste(
     "District: ", cd_2020, "\n",
-    "Hispanic population: ", round(prop_hisp, digits = 2),"%", "\n",
-    "White Population: ", round(prop_white, digits = 2), "%", "\n",
-    "Black Population: ", round(prop_black, digits = 2), "%", "\n",
-    "Native Population: ", round(prop_aian, digits = 2), "%", "\n",
-    "Asian Population: ", round(prop_asian, digits = 2), "%", "\n",
-    "Islander Population: ", round(prop_nhpi, digits = 2), "%", "\n",
-    "Other Population : ", round(prop_other, digits = 2), "%", "\n",
+    "Presidential Race\n",
+    "Joe Biden(D) vote percentage: ", round(prop_priz_20_bid, digits = 2),"%", "\n",
+    "Donald Trump(R) vote percentage: ", round(prop_priz_20_tru, digits = 2), "%", "\n",
+    "Senator Race\n",
+    "Mark Warner(D) vote percentage: ", round(prop_uss_20_war, digits = 2), "%", "\n",
+    "Daniel Gade(R) vote percentage: ", round(prop_uss_20_ga, digits = 2), "%", "\n",
     sep=""))) +
   geom_sf(color = "gray", size = 2) +
   scale_fill_party_c() +
-  theme_map()
+  theme_map() +
+  labs(title="Election for the state of Virgina in 2020",
+       subtitle= "Vote percentage of presidtial and sentor race") +
+  theme(plot.title = element_text(hjust = 0.5, size=15)) +
+  theme(plot.subtitle = element_text(hjust = 0.5))
+
 ggplotly(map_va_in, tooltip = "text") |>
   style(hoveron = "fill") 
 
