b96eea025482d00416929df5e317df980ad9abe2
Chando64 <famhernanrodri09@gmail.com>
2026-04-04 11:20:41 -0700

    Place graphs into html file to be placed in presentation.

--- Elections of two states.R
+++ Elections of two states.R
@@ -10,17 +10,32 @@ library(sf)
 map_va <- alarm_50state_map('VA')
 plans_va <- alarm_50state_plans('VA')
 
-map_va |> 
+map_va_1 <- map_va |>
   ggplot() +
   geom_district(aes(group = cd_2020, fill = ndv, denom = ndv + nrv)) +
-  geom_sf(fill = NA, color = "white", linewidth = 0.1, alpha = 0.3) + 
+  geom_sf(color = "gray", linewidth = 0.1, alpha = 0.3) + 
   scale_fill_party_c() +
   theme_map()
 
+ggplotly(map_va_1, tooltip = "text")
+
+
+
+map_va_1 <- map_va |>
+  ggplot() +
+  geom_sf(color = "gray", linewidth = 0.1, aes(group = cd_2020, fill = (ndv/(ndv+nrv)))) + 
+  geom_district(aes(group = cd_2020, fill = ndv, denom = ndv + nrv)) +
+  scale_fill_party_c() +
+  theme_map()
+
+ggplotly(map_va_1, tooltip = "text")
+
+
 
 map_va |> 
   ggplot() +
   geom_district(aes(group = cd_2020, fill = ndv, denom = ndv + nrv)) +
+  geom_sf(color = "gray", linewidth = 0.1, alpha = 0.3) + 
   scale_fill_party_c() +
   theme_map()
 
@@ -66,26 +81,3 @@ map_va_in <- popu_va_1 |>
 ggplotly(map_va_in, tooltip = "text") |>
   style(hoveron = "fill") 
 
-
-map_va_in1 <- popu_va_1 |>
-  ggplot(aes(fill = vote_share, text = paste(
-    "District: ", cd_2020, "\n",
-    "Presidential Race\n",
-    "Joe Biden(D) vote percentage: ", round(prop_priz_20_bid, digits = 2), "%", "\n",
-    "Donald Trump(R) vote percentage: ", round(prop_priz_20_tru, digits = 2), "%", "\n",
-    "Senator Race\n",
-    "Mark Warner(D) vote percentage: ", round(prop_uss_20_war, digits = 2), "%", "\n",
-    "Daniel Gade(R) vote percentage: ", round(prop_uss_20_ga, digits = 2), "%", "\n",
-    sep = ""
-  ))) +
-  geom_sf(data= map_va, color = "white", linewidth = 0.05, add = TRUE) + 
-  geom_district(aes(group = cd_2020), fill = NA, color = "black", linewidth = 0.6) +
-  scale_fill_party_c() +
-  theme_map() +
-  labs(
-    title = "Election for the state of Virginia in 2020",
-    subtitle = "Vote percentage of presidential and senator race"
-  )
-
-ggplotly(map_va_in1, tooltip = "text") |>
-  style(hoveron = "fill")
